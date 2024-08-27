7fd29d123e903136d3a3dafc1854bcafb8312758
Respublicanec <respublicanec37@mail.ru>
2024-08-27 10:55:18 +0300

    Init

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nuxt-link to="/about">ABOUT</nuxt-link>
+    <br>
+    <NuxtLink to="/">INDEXX</NuxtLink>
+    <br>
+    <NuxtPage></NuxtPage>
+  </div>
+</template>
+
+<script setup>
+const text = ref('')
+</script>
+
+<style>
+
+</style>
+
